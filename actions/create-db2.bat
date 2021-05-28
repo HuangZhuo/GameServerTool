b7088148d4faefcf108330e091951de982f02ab2
huangzhuo <idorable@163.com>
2021-05-28 14:03:33 +0800

    actions\create-db2.bat

---
+++ actions/create-db2.bat
@@ -0,0 +1,35 @@
+echo off
+path=%path%;D:\MTGame\game\runtime\MySQL51\bin
+
+set id=%1
+
+echo start create game+log database: %id%
+if "%id%"=="" (
+    echo empty args!
+    exit
+)
+
+set gamedb=octgame%id%
+set logdb=octlog%id%
+
+set gamesql=%~p0octgame.sql
+set logsql=%~p0octlog.sql
+
+echo show databases; > %~p0list-db.sql
+mysql -uroot -p123456 -P3310 < %~p0list-db.sql | findstr %gamedb% && echo %gamedb% already exists! || (
+    echo create database %gamedb%; > %~p0create-db.sql
+    mysql -uroot -p123456 -P3310 < %~p0create-db.sql
+    mysql -uroot -p123456 -P3310 %gamedb% < %gamesql%
+    echo %gamedb% is created!
+)
+
+mysql -uroot -p123456 -P3311 < %~p0list-db.sql | findstr %logdb% && echo %logdb% already exists! || (
+    echo create database %logdb%; > %~p0create-db.sql
+    mysql -uroot -p123456 -P3311 < %~p0create-db.sql
+    mysql -uroot -p123456 -P3311 %logdb% < %logsql%
+    echo %logdb% is created!
+)
+
+del %~p0list-db.sql /q
+del %~p0create-db.sql /q
+echo finished!
